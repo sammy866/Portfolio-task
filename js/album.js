4a9aabb85ebe9ef666538bd11acf9cf0674af7bd
sammy866 <62469628+sammy866@users.noreply.github.com>
2023-03-07 10:02:45 +0000

    Add files via upload

--- js/album.js
+++ js/album.js
@@ -1,28 +1,29 @@
 function loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
-      }
-    };
-    xmlhttp.open("GET", "album.xml", true);
-    xmlhttp.send();
-  }
-
-  function myFunction(xml) {
-    var x, i, xmlDoc, table;
-    xmlDoc = xml.responseXML;
-    table = "<tr><th>Artist</th><th>Title</th><th>Country</th></tr>";
-    x = xmlDoc.getElementsByTagName("CD");
-    for (i = 0; i < x.length; i++) {
-      table +=
-        "<tr><td>" +
-        x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-        x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-        x[i].getElementsByTagName("COUNTRY")[0].childNodes[0].nodeValue +
-        "</td></tr>";
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
     }
-    document.getElementById("demo").innerHTML = table;
+  };
+  xmlhttp.open("GET", "xml/album.xml", true);
+  xmlhttp.send();
+}
+
+function myFunction(xml) {
+  var x, i, xmlDoc, table;
+  xmlDoc = xml.responseXML;
+  table =
+    "<tr><th>Album</th><th>Release Year</th><th>Number of Songs</th></tr>";
+  x = xmlDoc.getElementsByTagName("CD");
+  for (i = 0; i < x.length; i++) {
+    table +=
+      "<tr><td>" +
+      x[i].getElementsByTagName("ALBUM")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("AMOUNT")[0].childNodes[0].nodeValue +
+      "</td></tr>";
   }
+  document.getElementById("albums").innerHTML = table;
+}
--- js/canvas.js
+++ js/canvas.js
@@ -2,6 +2,3 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0,0,150,75);
-
-
-let hello = "hello world"
