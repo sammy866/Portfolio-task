64ce61c512c619fb1a6909cf665c25dd43eee851
sammy866 <62469628+sammy866@users.noreply.github.com>
2023-03-07 12:02:27 +0000

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,43 +1,31 @@
 function validateForm() {
-    //Get form elements values
-    var firstName = document.forms["myForm"]["fname"].value.trim();
-    var lastName = document.forms["myForm"]["lname"].value.trim();
-    var email = document.forms["myForm"]["email"].value.trim();
-    var message = document.forms["myForm"]["message"].value.trim();
-  
-    //regular expression for email validation
-    var regEmail= /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-  
-    //checks Full Name
-    if (firstName == "") {
-      alert("First name must be filled out");
+    // Get the values of the form input fields
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    // Check if the required fields are empty
+    if (fname == "") {
+      alert("Please fill out your first name");
       return false;
     }
-     if (lastName== "")
-    {
-      alert("Last name must be filled out");
-      return false;
-    }
-  
-  //checks Email 
-    if(email == "")
-    {
-      alert("Please provide your e-mail.");
-      return false;
-    }
-  
-    if (!email.match(regEmail)) {
+
+    // Check if the email field is empty and meets the email format requirements
+    // This regular expression matches any string that contains one or more characters, followed by an "@" symbol, followed by one or more characters, followed by a ".",
+    // and then two or more characters (the domain)
+    var emailRegex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+    if (email == "" || !emailRegex.test(email)) {
       alert("Please enter a valid email address");
       return false;
     }
-  
-  //checks message
-    if(message == '') {
-      alert("Message cannot be empty!");
+
+    // Check if the message field is empty
+    if (message == "") {
+      alert("Please enter your message");
       return false;
     }
-  
-  //if everything is ok, the form submission will happen, and the function will return true.
-    
+
+    // If all validations pass, return true to allow the form to submit
     return true;
   }
