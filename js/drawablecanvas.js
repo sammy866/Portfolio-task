eaa3b8f139727aeb2bb2ef8a945aadad97f4aeb6
sammy866 <62469628+sammy866@users.noreply.github.com>
2023-03-16 13:11:49 +0000

    Add files via upload

--- js/drawablecanvas.js
+++ js/drawablecanvas.js
@@ -1,3 +1,4 @@
+/*
 window.addEventListener('load', () =>{
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext('2d');
@@ -31,4 +32,85 @@ window.addEventListener('load', () =>{
     canvas.addEventListener('mousedown', startPosition);
     canvas.addEventListener('mouseup', endPosition);
     canvas.addEventListener('mousemove', draw);
+});
+
+document.getElementById('clear').onclick = function(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+document.querySelectorAll(".colorChange").forEach((e) => {
+    e.onclick = function () {
+        context.strokeStyle = e.style.backgroundColor;
+    };
+});
+*/
+
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+let canvasOffsetX = canvas.offsetLeft;
+let canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+toolbar.addEventListener('change', e => {
+    if(e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if(e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+    
+});
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.beginPath(); // start a new path
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY - canvasOffsetY);
+    ctx.stroke();
+
+    startX = e.clientX - canvasOffsetX;
+    startY = e.clientY - canvasOffsetY;
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX - canvasOffsetX;
+    startY = e.clientY - canvasOffsetY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
+
+window.addEventListener('resize', () => {
+    canvasOffsetX = canvas.offsetLeft;
+    canvasOffsetY = canvas.offsetTop;
+    canvas.width = window.innerWidth - canvasOffsetX;
+    canvas.height = window.innerHeight - canvasOffsetY;
 });
